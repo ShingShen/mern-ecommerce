719790d9a6b8b3f0d4ec3bd9cf58698e4b62e33d
shingshen <seeeyetoeye314@gmail.com>
2021-05-15 23:51:38 +0800

    first version

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD6yVCiSrBo_BVFrHLQNoWlLvI7-uB0u-c",
+  authDomain: "mern-ecommerce-cc435.firebaseapp.com",
+  projectId: "mern-ecommerce-cc435",
+  storageBucket: "mern-ecommerce-cc435.appspot.com",
+  messagingSenderId: "887462622997",
+  appId: "1:887462622997:web:040f796e1b07ce5d041b1c",
+  measurementId: "G-L11QPQ2J94"
+};
